8b820fab80db390a9b61c0e9e424ef481b2b9b47
Val <vbonilla@fi.uba.ar>
2025-06-15 18:36:51 -0300

    guardo mejoras de PinguBooks.js

--- API/api.js
+++ API/api.js
@@ -1,16 +1,18 @@
 const express =require("express");
 const app = express();
-const {getAllPinguBooks} =require(".../db/PinguBooks");
+const {getAllPinguBooks, comparisonMail} =require(".../db/PinguBooks");
+const { createdUser } = require("./db/PiguBooks");
 const port=3000;
 app.use(express.json());
 
+
 //Organizar de cada uno el de autor, comentario y obra (GET, POST, DELETE y PUT)
 
-app.get("/", (req,res)=>{
+app.get("/inicio", (req,res)=>{
     res.send("");
 })
 
-//GET
+//GET 
 app.get( "", (req, res)=>{
     const pingu = getAllPinguBooks();
     res.json(pingu);
@@ -49,8 +51,41 @@ app.delete( "", (req, res)=>{
 app.put("", (req, res)=>{}
 
 )
+
+//EMPECEMOS POR PERFILES
+//VER PERFIL
+app.get("/perfil/num", (req,res)=>{
+    const response = req.params.num;
+})
+
+
+// CREAR PERFIL POST
+app.post("/",(req, res)=>{
+    const name =req.body.name;
+    const biography = req.body.biography;
+    const mail= req.body.mail;
+    const dateBirthday = req.body.dateBirthday;
+    const password= req.body.password; 
+    const averageRatingWorks=req.body.averageRatingWorks;
+    const dateLogIn=req.body.dateBirthday;
+    const country=req.body.country;
+    if((name===undefined) || (biography===undefined) || (mail===undefined) || (dateBirthday===undefined) || (password===undefined) ||(averageRatingWorks===undefined) || (dateLogIn===undefined) || (country===undefined)){
+        return res.sendStatus(404).send("Error al crear usuario. Todos los campos deben estar llenos");
+}
+    const validationMail = comparisonMail(mail);
+    if(validationMail !== 0){
+        return res.sendStatus(409).send("El mail otorgado ya esta en uso");
+        }
+ const userName = await createdUser(name, biography, mail, dateBirthday, password, averageRatingWorks, dateLogIn, country);
+
+})
+
+//crear obras
+app.post("/",(req, res)=>{
+const
+})
 app.listen(port, () => {
     console.log('Server listen port 3000');
   });
   
-})
+}) 
