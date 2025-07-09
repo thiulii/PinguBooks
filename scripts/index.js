edc86b5f9c71f35b9e279a45edb7dac7ba9377d1
Thiago <tiuliano@fi.uba.ar>
2025-07-09 17:13:51 -0300

    index terminado

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,59 @@
 BDD_LINK = "http://localhost:3000";
 
 async function cargarTop(){
-    const top = document.getElementById("top");
-    const res = await fetch(BDD_LINK + "/obras?by=puntuacion&limit=10&order=desc");
-    console.log("hola");
-    return "lol"
+
+    const res = await fetch(BDD_LINK + "/obras?by=puntuacion&limit=8&order=desc");
+    const obras = await res.json();
+
+    obras.forEach((libro, i) => {
+        const fila = document.createElement("a");
+        fila.href = `libro.html?id=${libro.id_obras}`;
+        fila.classList.add("libro", "container");
+        
+        const nombre = document.createElement("h7");
+        nombre.innerHTML = `TOP ${i + 1}: ${libro.titulo}`;
+        fila.appendChild(nombre);
+
+        let foto = document.createElement("img");
+        foto.src = libro.portada;
+        foto.alt = `portada de ${libro.titulo}`;
+        foto.classList.add("portadita")
+        fila.appendChild(foto);
+
+        const top = document.getElementById("top-tabla");
+        top.appendChild(fila);
+    });
 }
 
+async function cargarLatest(){
+
+    const res = await fetch(BDD_LINK + "/obras?by=fecha_de_publicacion&order=desc");
+    const obras = await res.json();
+
+    obras.forEach((libro) => {
+        const elemento = document.createElement("div");
+        elemento.classList.add("col-6", "col-md-4", "col-lg-3", "mb-3", "libro");
+        
+        const link = document.createElement("a");
+        link.href = `libro.html?id=${libro.id_obras}`;
+        elemento.appendChild(link);
+
+        const foto = document.createElement("img");
+        foto.src = libro.portada;
+        foto.alt = `portada de ${libro.titulo}`;
+        foto.classList.add("portada")
+        link.appendChild(foto);
+
+        const nombre = document.createElement("h5");
+        nombre.innerHTML = `${libro.titulo}`;
+        link.appendChild(nombre);
+
+        const top = document.getElementById("latest");
+        top.appendChild(elemento);
+    
+});
+}
 document.addEventListener("DOMContentLoaded", async function(){
     await cargarTop();
+    await cargarLatest();
 })
