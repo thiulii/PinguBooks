044e83798f9fe863f05f75057b2b1ba668fec829
Thiago <tiuliano@fi.uba.ar>
2025-07-10 00:01:45 -0300

    ahora puedo seleccionar y dejar seleccionado el criterio

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -10,34 +10,7 @@ if (tagsParam){
     tagsIniciales = tagsParam.split(",")
 }
 
-async function cargarCatalogo(){
-    console.log("entro al menos!")
-    const res = await fetch(BDD_LINK + "/obras?order=desc");
-    const obras = await res.json();
 
-    obras.forEach((libro) => {
-        const elemento = document.createElement("div");
-        elemento.classList.add("col-6", "col-md-3", "col-lg-2", "mb-3", "libro");
-        
-        const link = document.createElement("a");
-        link.href = `libro.html?id=${libro.id_obras}`;
-        elemento.appendChild(link);
-
-        const foto = document.createElement("img");
-        foto.src = libro.portada;
-        foto.alt = `portada de ${libro.titulo}`;
-        foto.classList.add("portada")
-        link.appendChild(foto);
-
-        const nombre = document.createElement("h5");
-        nombre.innerHTML = `${libro.titulo}`;
-        link.appendChild(nombre);
-
-        const biblioteca = document.getElementById("catalogoObras");
-        biblioteca.appendChild(elemento);
-    
-});
-}
 
 // tags seleccionados (agregar y sacar)
 
@@ -89,23 +62,65 @@ let mostrarOrden;
 function cargarFiltros(){
     if (!by || !["fecha_de_publicacion", "puntuacion"].includes(by)){
         criterioSeleccionado="fecha_de_publicacion";
+    } else {
+        criterioSeleccionado = by;
     }
-    if (criterioSeleccionado="fecha_de_publicacion"){
+    if (criterioSeleccionado==="fecha_de_publicacion"){
         mostrarOrden = "fecha de publicacion";
+    } else {
+        mostrarOrden = "estrellas"
     }
 
-    criterioBoton.forEach((boton) => {
-        
+    botonesCriterio.forEach((boton) => {
+        const criterio = boton.textContent.trim();
+        boton.addEventListener("click", () => {
+            if (criterio === "fecha de publicacion"){
+                criterioSeleccionado = "fecha_de_publicacion";
+                mostrarOrden = criterio;
+            } else{
+                criterioSeleccionado = "estrellas";
+                mostrarOrden = "puntuacion";
+            }
+            dropdownCriterio.innerText= `Criterio: ${mostrarOrden}`;
+          });
     })
 
     dropdownCriterio.innerText= `Criterio: ${mostrarOrden}`;
 }
 
 
+async function cargarCatalogo(){
+    console.log("entro al menos!")
+    const res = await fetch(BDD_LINK + "/obras?order=desc");
+    const obras = await res.json();
+
+    obras.forEach((libro) => {
+        const elemento = document.createElement("div");
+        elemento.classList.add("col-6", "col-md-3", "col-lg-2", "mb-3", "libro");
+        
+        const link = document.createElement("a");
+        link.href = `libro.html?id=${libro.id_obras}`;
+        elemento.appendChild(link);
+
+        const foto = document.createElement("img");
+        foto.src = libro.portada;
+        foto.alt = `portada de ${libro.titulo}`;
+        foto.classList.add("portada")
+        link.appendChild(foto);
+
+        const nombre = document.createElement("h5");
+        nombre.innerHTML = `${libro.titulo}`;
+        link.appendChild(nombre);
+
+        const biblioteca = document.getElementById("catalogoObras");
+        biblioteca.appendChild(elemento);
+    
+});
+}
 
 document.addEventListener("DOMContentLoaded", async function(){
 
     await cargarTags();
-    await cargarFiltros(order, by, tags); 
-    await cargarCatalogo(search, order, by, tags);
+    await cargarFiltros(); 
+    await cargarCatalogo();
 })
