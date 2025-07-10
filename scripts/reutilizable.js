1356405df1412c5c2e0e4260c8ef0158bd71e8ab
Thiago <tiuliano@fi.uba.ar>
2025-07-10 03:51:53 -0300

    navbar para buscar hecha, aunque solo aplicada a catalogo e index para evitar conflictos futuros al unir

--- scripts/reutilizable.js
+++ scripts/reutilizable.js
@@ -1,2 +1,11 @@
-formBuscar = document.getElementById("navbarBuscarForm");
-botonBuscar = document.getElementById("navbarBuscarBoton");
+const formBuscar = document.getElementById("navbarBuscarForm");
+const inputBuscar = document.getElementById("navbarBuscarInput");
+
+
+formBuscar.addEventListener("submit", function (e) {
+  e.preventDefault(); // Evita que recargue la página
+  const query = inputBuscar.value.trim();
+  if (query !== "") {
+    window.location.href = `/catalogo.html?search=${encodeURIComponent(query)}`;
+  }
+});
