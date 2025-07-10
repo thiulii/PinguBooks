c7d507d05852c93758dba8184d2d319de4eca4de
Val <vbonilla@fi.uba.ar>
2025-07-10 14:09:34 -0300

    Agrego todo lo de perfil autor, con estrella en puntuacion

--- Backend/PinguBooks.js
+++ Backend/PinguBooks.js
@@ -28,8 +28,8 @@ async function getAllAutores() {
 }
 async function sumAndAverage(id_autor) {
   try{
-    const resultado = await dbPinguBooks.query("SELECT COUNT(*) AS cant_obras, ROUND(AVG(puntuacion)) AS prom_puntuacion FROM obras WHERE id_autor= $1; ", [id_autor]);
-    return resultado.row[0];}
+    const resultado = await dbPinguBooks.query("SELECT COUNT(*) AS cant_obras, AVG(puntuacion) AS prom_puntuacion FROM obras WHERE id_autor= $1; ", [id_autor]);
+    return resultado.rows[0];}
   catch(err){
     console.error("error:", err);
     return undefined;
